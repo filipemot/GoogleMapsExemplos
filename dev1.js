c94004a94b7dab07394c15be627e7ea021350dd8
tria.luism <spcbrasil.2021>
2021-07-21 10:57:35 -0300

    Carregando Ponto ao Iniciar o Mapa

--- dev1.js
+++ dev1.js
@@ -1,10 +1,17 @@
 let map;
-
+let wktPonto = "POINT(150.501177734375 -34.54590788786563)"
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+  
+  var wkt = new Wkt.Wkt();
+
+// Read in any kind of WKT string
+wkt.read(wktPonto);
+let poligono = wkt.toObject();
+poligono.setMap(map);
   
   google.maps.event.addListener(map, "idle", function() {
    let latNE = map.getBounds().getNorthEast().lat();
